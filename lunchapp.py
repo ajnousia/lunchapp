33a33d0d1e6bb71b0bae8e538d952668fc139c9b
Riikka <rhnousiainen@gmail.com>
2015-08-30 14:28:45 +0300

    bootstrap lisätty

--- lunchapp.py
+++ lunchapp.py
@@ -5,6 +5,9 @@ import urllib2
 import webapp2
 import datetime
 
+bootstrap = ["""<link rel="stylesheet" href="/bootstrap/css/bootstrap.css">""",
+          """<link rel="stylesheet" href="/bootstrap/css/bootstrap-responsive.css">""",
+          """<script src="bootstrap/js/jquery.js"></script>"""]
 def parse_json(url):
     request = urllib2.urlopen(url)
     
@@ -18,11 +21,11 @@ def parse_json(url):
     
     for menu in bolero_menus:
         date = datetime.datetime.strptime(menu["Date"],'%Y-%m-%dT%H:%M:%S')
-        menu_string += "<h1>{0}</h1><br>".format(date.strftime('%A %d.%m.'))
+        menu_string += "<h1>{0}</h1>".format(date.strftime('%A %d.%m.'))
         
         for index, lunch in enumerate(menu["SetMenus"]):
             if lunch["Name"] != None:
-                menu_string += "<p>%s<br>" % lunch["Name"]
+                menu_string += "<h2>%s</h2><p>" % lunch["Name"]
             for component in  lunch["Components"]:
                 menu_string += "%s<br>" % component
             menu_string += "<i>%s</i></p>" % lunch["Price"]
@@ -31,9 +34,11 @@ def parse_json(url):
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        #self.response.headers['Content-Type'] = 'text/plain; charset=utf-8'
-        #self.response.write("""<link rel="stylesheet" href="/bootstrap/css/bootstrap.css">""")
-        #self.response.write("""<link rel="stylesheet" href="/bootstrap/css/bootstrap-responsive.css">""")
+        self.response.headers['Content-Type'] = 'text/html; charset=utf-8'
+        self.response.out.write("<html>")
+        for style in bootstrap:
+            self.response.out.write(style)
+        self.response.out.write("<body>")
         self.response.out.write("<html><body>")
         self.response.out.write('Menus for this week:\n{1}'.format(today, bolero.encode('utf-8')))
         self.response.out.write("</body></html>")

12df4ede8064ddc741783370ba1142fc3c27ddeb
Riikka <rhnousiainen@gmail.com>
2015-08-30 12:52:52 +0300

    ääkköset korjattu, lisätty pvm

--- lunchapp.py
+++ lunchapp.py
@@ -4,28 +4,35 @@ import json
 import urllib2
 import webapp2
 import datetime
-#import dateutil.parser
 
 def parse_json(url):
+    request = urllib2.urlopen(url)
+    
+    #encoding = request.headers['content-type'].split('charset=')[-1]
+    #ucontent = unicode(content, encoding)
+    
     # load the object from a string
-    json_obj = json.loads(urllib2.urlopen(url).read())
+    json_obj = json.loads(request.read())
     bolero_menus = json_obj["MenusForDays"]
     menu_string = ""
     
     for menu in bolero_menus:
-        #date = dateutil.parser.parse(menu["Date"]).date()
-        #menu_string += "{0}.{1}.{2}\n".format(date.day, date.month, date.year)
+        date = datetime.datetime.strptime(menu["Date"],'%Y-%m-%dT%H:%M:%S')
+        menu_string += "{0}\n\n".format(date.strftime('%A %d.%m.'))
+        
         for index, lunch in enumerate(menu["SetMenus"]):
-            menu_string += "Menu %s\n" % str(index)
+            if lunch["Name"] != None:
+                menu_string += "%s\n" % lunch["Name"]
             for component in  lunch["Components"]:
                 menu_string += "%s\n" % component
+            menu_string += "%s\n\n" % lunch["Price"]
 
     return menu_string
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World! It is {0}\nMenus for this week:\n{1}'.format(today, unicode(bolero).encode('utf8')))
+        self.response.headers['Content-Type'] = 'text/plain; charset=utf-8'
+        self.response.write('Menus for this week:\n{1}'.format(today, bolero.encode('utf-8')))
 
 
 today = datetime.date.isoformat(datetime.date.today())

41703d7109e3564e140c03721dc3a44d9edbdc1f
Riikka <rhnousiainen@gmail.com>
2015-10-18 12:00:19 +0300

    data fetch refaktoroitu ja logging lisätty

--- unclassified_functions.py
+++ unclassified_functions.py
@@ -10,36 +10,31 @@ def fetch_restaurants_data():
     today = datetime.date.today()
     last_monday = today + datetime.timedelta(days=-today.weekday(), weeks=0)
     restaurants = Restaurants()
-    try:
-        bolero = Restaurant("Bolero", Address("Atomitie", "2", "00370", "Helsinki"))
-        for menu in parse_bolero_json(last_monday):
-            bolero.add_day_menu(menu)
-        restaurants.add_restaurant(bolero)
-    except Exception:
-        pass
-    try:
-        atomitie5 = Restaurant("Atomitie 5", Address("Atomitie", "5", "00370", "Helsinki"))
-        for menu in parse_atomitie5_json(last_monday):
-            atomitie5.add_day_menu(menu)
-        restaurants.add_restaurant(atomitie5)
-    except Exception:
-        pass
-    try:
-        picante = Restaurant("Picante", Address("Valimotie", "8", "00380", "Helsinki"))
-        for menu in parse_picante_html():
-            picante.add_day_menu(menu)
-        restaurants.add_restaurant(picante)
-    except Exception:
-        pass
+
+    bolero = Restaurant("Bolero", Address("Atomitie", "2", "00370", "Helsinki"))
+    restaurants = add_restaurant_data(restaurants, bolero, parse_bolero_json, last_monday)
+
+    atomitie5 = Restaurant("Atomitie 5", Address("Atomitie", "5", "00370", "Helsinki"))
+    restaurants = add_restaurant_data(restaurants, atomitie5, parse_atomitie5_json, last_monday)
+
+    picante = Restaurant("Picante", Address("Valimotie", "8", "00380", "Helsinki"))
+    restaurants = add_restaurant_data(restaurants, picante, parse_picante_html)
+
     sellon_ravintolat = ["marian-konditoria", "ravintola-base", "cafe-buffo", "ravintola-retro", "rax-buffet", "glo-grill-kitchen", "chicos"]
     for name in sellon_ravintolat:
-        restaurant = Restaurant(name.replace("-"," ").replace("ravintola","").strip().title(), None)
-        try:
-            for menu in parse_sello_html(name, last_monday):
-                restaurant.add_day_menu(menu)
-            restaurants.add_restaurant(restaurant)
-        except Exception as error:
-            pass    
+        sellon_ravintola = Restaurant(name.replace("-"," ").replace("ravintola","").strip().title(), None)
+        restaurants = add_restaurant_data(restaurants, sellon_ravintola, parse_sello_html, name, last_monday)
+
+    return restaurants
+
+def add_restaurant_data(restaurants, restaurant, parse_function, *args):
+    try:
+        for menu in parse_function(*args):
+            restaurant.add_day_menu(menu)
+        restaurants.add_restaurant(restaurant)
+    except Exception as error:
+        logging.error('There was an error parsing restaurant data: %s' % error)
+        pass
     return restaurants
 
 

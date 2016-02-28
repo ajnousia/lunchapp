8d1d3142ca2f3de44ae6905773e9016d3465d058
Antti Nousiainen <ajnousia@gmail.com>
2016-02-28 15:47:28 +0200

    Implemented backup_PickledRestaurants_entities_locally(batch_size):

---
+++ datastore_housekeeping_functions.py
@@ -0,0 +1,15 @@
+import datetime
+import lunchapp
+import logging
+import data_store_classes
+import pickle
+
+
+def add_dates_to_datastore_entities():
+    pass
+
+def backup_PickledRestaurants_entities_locally(batch_size):
+    pickled_restaurants = data_store_classes.PickledRestaurants.query().fetch(batch_size)
+    file_name = "PickledRestaurants_backup_"+ datetime.date.today().isoformat()
+    with open(file_name, "wb") as output:
+        pickle.dump(pickled_restaurants, output, -1)

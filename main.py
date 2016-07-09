fe9dabb92d2dde36f88f866bba623654f46c05e6
Antti Nousiainen <ajnousia@gmail.com>
2016-07-09 13:05:17 +0300

    New backend initial implementation

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python
 # -- coding: utf-8 --
-import webapp2
+
 from app.lunchapp import *
+from material_backend.materiala import *
+
+print "ASDF"
+asdf()
+
+
 # Kun depolyaat appengineen:
-# 1. Vaihda debug=False
+# 1. Vaihda debug=Fals
 
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
     (r'/settings', SettingsPage),
     (r'/about', AboutPage),
-    (r'/worker', FetchData)
+    (r'/worker', FetchData),
+    ('/rest/query', QueryHandler),
     ],
     debug=False)
